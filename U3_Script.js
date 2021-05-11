386b6d7c6546050900ad152338d8e851fd921094
Simon Meissner <simon.meissner1@gmx.net>
2021-05-11 21:29:42 +0200

    final changes

--- U3_Script.js
+++ U3_Script.js
@@ -3,7 +3,7 @@
 
 //defining variables
 
-var coordinates
+var coordinates // coordinates holds the coordinates of the requested position
 
 //defining functions
 
@@ -13,11 +13,14 @@ var coordinates
    */
   function getWeatherData () {
 
+    //success function when accessing position is accepted
     function success(position) {
       coordinates = position.coords
 
       let xmlreq = new XMLHttpRequest();
 
+      //XML-HTTP Request to get the weatherinformation from https://openweathermap.org/
+      //parsing it to JSON and invoking displayWeatherAtPosition()
       xmlreq.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
           let weatherData = JSON.parse(this.responseText)
@@ -29,11 +32,11 @@ var coordinates
       xmlreq.send();
     }
 
-
+    // Error function when accessing position is denied
     function error(err) {
       console.warn(`Warn(${err.code}) : ${err.message}`)
     }
-
+    // getting the position from the geolocation api
     navigator.geolocation.getCurrentPosition(success, error)
   }
 
@@ -45,7 +48,7 @@ var coordinates
    */
   function displayWeatherAtPosition(weatherData) {
       //prints given location
-      document.getElementById("location").textContent = "Your location: " + weatherData.lat + " " + weatherData.lon
+      document.getElementById("location").textContent = "Your location: Lat:" + weatherData.lat + ", Lon:" + weatherData.lon
 
       //prints given date
       const options = {weekday: 'long', year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric'}
@@ -67,12 +70,19 @@ var coordinates
       let feellikeC = Math.round(feellike - 273.15)
       document.getElementById("feellike").textContent = "Feels like: " + feellikeK + "K/" + feellikeC + "°C"
 
+      //prints windspeed
+      document.getElementById("windspeed").textContent = "Windspeed: " + weatherData.current.wind_speed + "meter/sec"
+
       //prints humidity
       document.getElementById("humidity").textContent = "Humidity: " + weatherData.current.humidity + "%"
       //prints pressure
       document.getElementById("pressure").textContent = "Pressure at Sealevel: " + weatherData.current.pressure + " hPa"
 
-
+      //prints sunrise and sunset
+      const options2 = {hour: 'numeric', minute: 'numeric', second: 'numeric'}
+      let sunrise = new Date(weatherData.current.sunrise * 1000).toLocaleTimeString("EN-EN", options2)
+      let sunset = new Date(weatherData.current.sunset * 1000).toLocaleTimeString("EN-EN", options2)
+      document.getElementById("sunriseandset").textContent = "Sunrise: " + sunrise + " Sunset: " + sunset
 
 
       console.log(weatherData) // testing
